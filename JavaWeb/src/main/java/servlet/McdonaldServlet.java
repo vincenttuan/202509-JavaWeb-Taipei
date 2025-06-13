83361975358cd7eecdefa6daa1089fc6992e5ff0
teacher <teacher@DESKTOP-TDDQARH>
2025-06-13 21:10:50 +0800

    j

---
+++ JavaWeb/src/main/java/servlet/McdonaldServlet.java
@@ -0,0 +1,23 @@
+package servlet;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/mcdonald")
+public class McdonaldServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html;charset=UTF-8");
+		
+		String hamburger
+		String 
+		
+	}
+	
+}
