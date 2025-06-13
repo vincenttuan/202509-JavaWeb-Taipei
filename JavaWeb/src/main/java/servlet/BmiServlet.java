1512408f9aced1723d999a113f01aa4aad51d598
teacher <teacher@DESKTOP-TDDQARH>
2025-06-13 19:47:55 +0800

    web

---
+++ JavaWeb/src/main/java/servlet/BmiServlet.java
@@ -0,0 +1,9 @@
+package servlet;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+@WebServlet("/bmi_servlet")
+public class BmiServlet extends HttpServlet {
+	
+}
