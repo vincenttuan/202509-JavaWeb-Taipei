15d2871b436c1af990ac479dda84afb3d54b05c9
teacher <teacher@DESKTOP-TDDQARH>
2025-06-13 19:59:49 +0800

    j

--- JavaWeb/src/main/java/servlet/BmiServlet.java
+++ JavaWeb/src/main/java/servlet/BmiServlet.java
@@ -14,6 +14,16 @@ public class BmiServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		resp.setContentType("text/html;charset=UTF-8"); // 告知瀏覽器回應內容的型態
+		
+		// 接收參數(height=170.5&weight=60.5)
+		String height = req.getParameter("height");
+		String weight = req.getParameter("weight");
+		// 檢查是否有輸入參數 ?
+		if(height == null || weight == null) {
+			resp.getWriter().print("請輸入身高/體重"); // 在瀏覽器上印出資料
+			return;
+		}
+		
 		resp.getWriter().print("BMI Result"); // 在瀏覽器上印出資料
 	}
 	
