e3a2ba4e92e38849f82c4ae9c3f49bc64c985a02
teacher <teacher@DESKTOP-TDDQARH>
2025-06-20 21:34:58 +0800

    web

--- JavaWeb/src/main/java/servlet/RegServlet.java
+++ JavaWeb/src/main/java/servlet/RegServlet.java
@@ -9,7 +9,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet("/JavaWeb/reg")
+@WebServlet("/reg")
 public class RegServlet extends HttpServlet {
 
 	@Override
