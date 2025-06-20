36f1e5815ab7b8f05d0e549def75c117e39faac4
teacher <teacher@DESKTOP-TDDQARH>
2025-06-20 19:52:58 +0800

    web

--- JavaWeb/src/main/java/model/Breakfast.java
+++ JavaWeb/src/main/java/model/Breakfast.java
@@ -1,11 +1,16 @@
 package model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Breakfast {
 	private String id; // B01
 	private String name; // 漢堡
 	private Integer price; // 40
 	private Category category; // 分類
-	
-	
 	
 }
