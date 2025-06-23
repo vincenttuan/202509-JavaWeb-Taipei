ab1a2525791cd0d508222a860b804d7f3de78265
teacher <teacher@DESKTOP-TDDQARH>
2025-06-23 19:16:11 +0800

    web

---
+++ JavaWeb/src/main/java/servlet/GuestbookServlet.java
@@ -0,0 +1,22 @@
+package servlet;
+
+import java.io.IOException;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/guestbook")
+public class GuestbookServlet extends HttpServlet {
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// 調用 guestbook.jsp
+		RequestDispatcher rd = req.getRequestDispatcher("guestbook.jsp");
+		rd.forward(req, resp);
+	}
+	
+}
