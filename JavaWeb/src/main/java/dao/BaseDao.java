a92927697545c4fe5ca3d95ab6415980d0e19ae0
teacher <teacher@DESKTOP-TDDQARH>
2025-06-23 21:32:28 +0800

    web

--- JavaWeb/src/main/java/dao/BaseDao.java
+++ JavaWeb/src/main/java/dao/BaseDao.java
@@ -14,6 +14,7 @@ public class BaseDao {
 				String username = "root";
 				String password = "12345678";
 				String jdbcUrl = "jdbc:mysql://localhost:3306/web?useUnicode=true&characterEncoding=UTF-8&serverTimezone=Asia/Taipei&useSSL=false";
+				//String jdbcUrl = "jdbc:mysql://localhost:3306/guestbook_db?useUnicode=true&characterEncoding=UTF-8&serverTimezone=Asia/Taipei&useSSL=false&allowPublicKeyRetrieval=true";
 				conn = DriverManager.getConnection(jdbcUrl, username, password);
 			} catch (Exception e) {
 				e.printStackTrace();
