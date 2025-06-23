83ebd2999709aa28de875343da326916df64e0f3
teacher <teacher@DESKTOP-TDDQARH>
2025-06-23 21:02:25 +0800

    web

--- JavaWeb/src/main/java/model/Guestbook.java
+++ JavaWeb/src/main/java/model/Guestbook.java
@@ -6,6 +6,17 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+/**
+ -- 建立 guestbook 資料表
+use web; -- 使用 web 資料庫
+create table guestbook (
+	id int auto_increment primary key,
+    name varchar(50) not null,
+    message varchar(255) not null,
+    create_at timestamp default current_timestamp
+);
+*/
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
