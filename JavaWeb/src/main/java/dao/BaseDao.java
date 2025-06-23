958f9b21da681468da08ad02e6921b467f370aed
teacher <teacher@DESKTOP-TDDQARH>
2025-06-23 21:10:44 +0800

    web

---
+++ JavaWeb/src/main/java/dao/BaseDao.java
@@ -0,0 +1,27 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class BaseDao {
+	
+	private static Connection conn;
+	
+	public Connection getConnection() {
+		if(conn == null) {
+			try {
+				Class.forName("com.mysql.cj.jdbc.Driver");
+				String username = "root";
+				String password = "12345678";
+				String jdbcUrl = "jdbc:mysql://localhost:3306/web?useUnicode=true&characterEncoding=UTF-8&serverTimezone=Asia/Taipei&useSSL=false";
+				conn = DriverManager.getConnection(jdbcUrl, username, password);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return conn;
+	}
+	
+	
+}
