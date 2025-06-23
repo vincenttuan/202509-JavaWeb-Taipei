149ea253db5c739ecfeffc503eee2db956a15398
teacher <teacher@DESKTOP-TDDQARH>
2025-06-23 19:29:33 +0800

    web

--- JavaWeb/src/main/java/dao/GuestbookDao.java
+++ JavaWeb/src/main/java/dao/GuestbookDao.java
@@ -1,5 +1,10 @@
 package dao;
 
+import java.util.List;
+
+import model.Guestbook;
+
 public interface GuestbookDao {
-	
+	public List<Guestbook> findAll(); // 查詢所有訪客留言資料
+	public void add(Guestbook guestbook); // 新增訪客留言資料
 }
