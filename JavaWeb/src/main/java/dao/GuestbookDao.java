8720e323596178d89678d7b4c5630aa8655041e3
teacher <teacher@DESKTOP-TDDQARH>
2025-06-27 19:58:27 +0800

    web

--- JavaWeb/src/main/java/dao/GuestbookDao.java
+++ JavaWeb/src/main/java/dao/GuestbookDao.java
@@ -8,4 +8,6 @@ public interface GuestbookDao {
 	public List<Guestbook> findAll(); // 查詢所有訪客留言資料
 	public void add(String name, String message); // 新增訪客留言資料
 	public void delete(Integer id); // 刪除指定留言
+	public Guestbook get(Integer id); // 取得單筆資料
+	public void update(Integer id, String name, String message); // 修改資料
 }
