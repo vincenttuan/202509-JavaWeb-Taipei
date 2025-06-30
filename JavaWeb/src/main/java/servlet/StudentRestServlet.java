a4ef2aab2398ecc3172a65b127eaaf55535fcd14
teacher <teacher@DESKTOP-TDDQARH>
2025-06-30 19:59:44 +0800

    web

--- JavaWeb/src/main/java/servlet/StudentRestServlet.java
+++ JavaWeb/src/main/java/servlet/StudentRestServlet.java
@@ -2,6 +2,8 @@ package servlet;
 
 import java.io.IOException;
 
+import com.google.gson.Gson;
+
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -12,13 +14,15 @@ import model.Student;
 @WebServlet("/rest/student")
 public class StudentRestServlet extends HttpServlet {
 	
+	private Gson gson = new Gson();
+	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		Student[] students = {
 				new Student("John", 19), new Student("Mary", 20)
 		};
 		
-		String json = "???";
+		String json = gson.toJson(students);
 		
 		resp.setContentType("application/json;charset=UTF-8");
 		resp.getWriter().print(json);
