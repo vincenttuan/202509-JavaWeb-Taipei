7da733e0c675eed931e427d0365a8244d5c5ae87
teacher <teacher@DESKTOP-TDDQARH>
2025-06-30 19:56:50 +0800

    web

--- JavaWeb/src/main/java/servlet/StudentRestServlet.java
+++ JavaWeb/src/main/java/servlet/StudentRestServlet.java
@@ -7,24 +7,19 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import model.Student;
 
 @WebServlet("/rest/student")
 public class StudentRestServlet extends HttpServlet {
 	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		String json = """
-						[
-							{
-								"name": "John",
-								"age": 19 
-							},
-							{
-								"name": "Mary",
-								"age": 20 
-							}
-						]
-					  """;
+		Student[] students = {
+				new Student("John", 19), new Student("Mary", 20)
+		};
+		
+		String json = "???";
+		
 		resp.setContentType("application/json;charset=UTF-8");
 		resp.getWriter().print(json);
 	}
