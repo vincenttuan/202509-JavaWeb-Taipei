4ceafaf8fcb5c72d69c0cc0b5d2754498dbb55d9
teacher <teacher@DESKTOP-TDDQARH>
2025-07-04 19:30:23 +0800

    web

---
+++ JavaWeb/src/main/java/servlet/ReportServlet.java
@@ -0,0 +1,32 @@
+package servlet;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+@WebServlet
+public class ReportServlet extends HttpServlet {
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html;charset=UTF-8");
+		
+		HttpSession session = req.getSession(false);
+		
+		if(session == null) {
+			resp.getWriter().print("此為會員專屬報告請先登入!");
+			return;
+		}
+		// 此網頁必須要有會員登入才能看
+		resp.getWriter().print("會員專屬報告:<p />");
+		resp.getWriter().print("明天放假一天");
+		
+		
+	}
+	
+}
