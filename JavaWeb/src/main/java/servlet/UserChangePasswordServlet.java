0c5285d1e3943f58de6a31c9e80e61d96e276049
teacher <teacher@DESKTOP-TDDQARH>
2025-07-07 21:38:03 +0800

    w

--- JavaWeb/src/main/java/servlet/UserChangePasswordServlet.java
+++ JavaWeb/src/main/java/servlet/UserChangePasswordServlet.java
@@ -2,6 +2,8 @@ package servlet;
 
 import java.io.IOException;
 
+import dao.UserDao;
+import dao.UserDaoMySQL;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -12,6 +14,8 @@ import jakarta.servlet.http.HttpSession;
 
 @WebServlet("/user/change/password")
 public class UserChangePasswordServlet extends HttpServlet {
+	
+	private UserDao userDao = new UserDaoMySQL();
 	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -31,5 +35,23 @@ public class UserChangePasswordServlet extends HttpServlet {
 		RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/view/update_password.jsp");
 		rd.forward(req, resp);
 	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html;chatset=UTF-8");
+		String oldPassword = req.getParameter("oldPassword");
+		String newPassword = req.getParameter("newPassword");
+		
+		HttpSession session = req.getSession(false);
+		String username = session.getAttribute("username").toString();
+		
+		// 1. 請先確認 oldPassword 是否正確(Homework)
+		
+		// 2. 進行密碼
+		userDao.updatePassword(username, newPassword);
+		resp.getWriter().print("密碼修改成功 !");
+		
+	}
+	
 	
 }
