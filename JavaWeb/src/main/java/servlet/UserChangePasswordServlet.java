a29f309ab22a2c8f6f8d47237d5224f9507e6978
teacher <teacher@DESKTOP-TDDQARH>
2025-07-07 21:16:40 +0800

    web

---
+++ JavaWeb/src/main/java/servlet/UserChangePasswordServlet.java
@@ -0,0 +1,11 @@
+package servlet;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+@WebServlet("/user/change/password")
+public class UserChangePasswordServlet extends HttpServlet {
+	
+	
+	
+}
