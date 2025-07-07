cafc6a72a7e1cbcff6477c221eb54adc726f37af
teacher <teacher@DESKTOP-TDDQARH>
2025-07-07 21:29:31 +0800

    web

--- JavaWeb/src/main/java/dao/UserDao.java
+++ JavaWeb/src/main/java/dao/UserDao.java
@@ -7,4 +7,5 @@ import model.User;
 public interface UserDao {
 	public User findByUsername(String username);
 	public List<User> findAllUsers();
+	public void updatePassword(String username, String newPassword);
 }
