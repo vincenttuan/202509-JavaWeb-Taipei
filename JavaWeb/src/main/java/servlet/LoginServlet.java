fe5273250438377fd3c31c702a4fecc0b11b1191
teacher <teacher@DESKTOP-TDDQARH>
2025-07-07 20:54:18 +0800

    web

--- JavaWeb/src/main/java/servlet/LoginServlet.java
+++ JavaWeb/src/main/java/servlet/LoginServlet.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
+import dao.UserDao;
+import dao.UserDaoMySQL;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -16,18 +18,7 @@ import util.PasswordHash;
 @WebServlet("/login")
 public class LoginServlet extends HttpServlet {
 	
-	private static List<User> users;
-	
-	static {
-		users = new CopyOnWriteArrayList<>();
-		// 請先建立 3 個使用者其中 salt 與 hash 請透過 PasswordHash.java 得到
-		// 1.帳號: admin, 密碼: 1234, salt: oKbdpbcvQqP+Oa+Uy0XjdA==, hash: e8c929460c099c457a06a70050e5b0e339bf3cd9841acb2ce57765a1b837f856
-		// 2.帳號: john,  密碼: 1234, salt: rLky4w7gGHvtwy7TCklMOA==, hash: 6d894de2d714f161cc943c841717f5dfb1c37f63b235e1e6efe0d49559affb8e
-		// 3.帳號: mary,  密碼: 5678, salt: FZWu03S4tEt/S1gZ63J89Q==, hash: 2fdaed2d97b89351858d82a6358cb8f6e759ef1e7c1bb59d783851f542f5abcb
-		users.add(new User("admin", "oKbdpbcvQqP+Oa+Uy0XjdA==", "e8c929460c099c457a06a70050e5b0e339bf3cd9841acb2ce57765a1b837f856"));
-		users.add(new User("john", "rLky4w7gGHvtwy7TCklMOA==", "6d894de2d714f161cc943c841717f5dfb1c37f63b235e1e6efe0d49559affb8e"));
-		users.add(new User("mary", "FZWu03S4tEt/S1gZ63J89Q==", "2fdaed2d97b89351858d82a6358cb8f6e759ef1e7c1bb59d783851f542f5abcb"));
-	}
+	private UserDao userDao = new UserDaoMySQL();
 	
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -41,10 +32,7 @@ public class LoginServlet extends HttpServlet {
 		HttpSession session = req.getSession();
 		
 		// 判斷帳號 username
-		User user = users.stream()
-				.filter(u -> u.getUsername().equals(username))
-				.findFirst()
-				.orElse(null);
+		User user = userDao.findByUsername(username);
 		
 		if(user == null) {
 			// 登入失敗
