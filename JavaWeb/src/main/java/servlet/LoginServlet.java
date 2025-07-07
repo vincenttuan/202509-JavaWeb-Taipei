1b4af68d6c6fdc80d09ef0fddead920cf1ff9a3d
teacher <teacher@DESKTOP-TDDQARH>
2025-07-07 19:53:54 +0800

    web

--- JavaWeb/src/main/java/servlet/LoginServlet.java
+++ JavaWeb/src/main/java/servlet/LoginServlet.java
@@ -23,7 +23,9 @@ public class LoginServlet extends HttpServlet {
 		// 1.帳號: admin, 密碼: 1234, salt: oKbdpbcvQqP+Oa+Uy0XjdA==, hash: e8c929460c099c457a06a70050e5b0e339bf3cd9841acb2ce57765a1b837f856
 		// 2.帳號: john,  密碼: 1234, salt: rLky4w7gGHvtwy7TCklMOA==, hash: 6d894de2d714f161cc943c841717f5dfb1c37f63b235e1e6efe0d49559affb8e
 		// 3.帳號: mary,  密碼: 5678, salt: FZWu03S4tEt/S1gZ63J89Q==, hash: 2fdaed2d97b89351858d82a6358cb8f6e759ef1e7c1bb59d783851f542f5abcb
-		
+		users.add(new User("admin", "oKbdpbcvQqP+Oa+Uy0XjdA==", "e8c929460c099c457a06a70050e5b0e339bf3cd9841acb2ce57765a1b837f856"));
+		users.add(new User("john", "rLky4w7gGHvtwy7TCklMOA==", "6d894de2d714f161cc943c841717f5dfb1c37f63b235e1e6efe0d49559affb8e"));
+		users.add(new User("mary", "FZWu03S4tEt/S1gZ63J89Q==", "2fdaed2d97b89351858d82a6358cb8f6e759ef1e7c1bb59d783851f542f5abcb"));
 	}
 	
 	@Override
