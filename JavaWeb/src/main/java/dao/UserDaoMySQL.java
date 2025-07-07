8327cec6224ca9e5450b3d20871286a96d765758
teacher <teacher@DESKTOP-TDDQARH>
2025-07-07 20:49:21 +0800

    web

---
+++ JavaWeb/src/main/java/dao/UserDaoMySQL.java
@@ -0,0 +1,40 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import model.User;
+
+public class UserDaoMySQL extends BaseDao implements UserDao {
+
+	@Override
+	public User findByUsername(String username) {
+		String sql = "select username, salt, hash from user where username = ?";
+		try(PreparedStatement pstmt = getConnection().prepareStatement(sql)) {
+			pstmt.setString(1, username);
+			
+			try(ResultSet rs = pstmt.executeQuery()) {
+				if(rs.next()) {
+					User user = new User();
+					user.setUsername(rs.getString("username"));
+					user.setSalt(rs.getString("salt"));
+					user.setHash(rs.getString("hash"));
+					return user;
+				}
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	@Override
+	public List<User> findAllUsers() {
+		return null;
+	}
+	
+}
