a4b7d4f34e8b455f2809e926d138541693a94147
teacher <teacher@DESKTOP-TDDQARH>
2025-07-07 19:28:35 +0800

    web

--- JavaWeb/src/main/java/util/PasswordHash.java
+++ JavaWeb/src/main/java/util/PasswordHash.java
@@ -2,16 +2,28 @@ package util;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.Base64;
 
 // 本段程式要實現 SHA-256 雜湊生成與比對
 public class PasswordHash {
 	
+	// 產生隨機 salt
+	public static String generateSalt() {
+		SecureRandom sr = new SecureRandom();
+		byte[] salt = new byte[16];
+		sr.nextBytes(salt);
+		return Base64.getEncoder().encodeToString(salt);
+	}
+	
 	// 密碼雜湊函式
-	public static String getHashPassword(String password) {
+	public static String getHashPassword(String password, String salt) {
 		
 		try {
 			// 建立一個 SHA-256 的雜湊演算法實例
 			MessageDigest md = MessageDigest.getInstance("SHA-256");
+			// 先加鹽
+			md.update(salt.getBytes());
 			// 將密碼字串轉成位元組陣列並進行雜湊運算
 			byte[] hashBytes = md.digest(password.getBytes());
 			// 建立一個 StringBuilder 用來組合雜湊後的十六進位字串
@@ -31,20 +43,21 @@ public class PasswordHash {
 	}
 	
 	// 比對
-	public static boolean checkPassword(String inputPassword, String storedHash) {
-		String inputHash = getHashPassword(inputPassword);
+	public static boolean checkPassword(String inputPassword, String salt, String storedHash) {
+		String inputHash = getHashPassword(inputPassword, salt);
 		return inputHash.equals(storedHash);
 	}
 	
 	
 	public static void main(String[] args) {
 		String password = "1234";
-		String storePassword = getHashPassword(password);
+		String salt = generateSalt();
+		String storePassword = getHashPassword(password, salt);
 		System.out.println(storePassword);
 		System.out.println(storePassword.length());
 		
 		String inputPassword = "1234";
-		boolean check = checkPassword(inputPassword, storePassword);
+		boolean check = checkPassword(inputPassword, salt, storePassword);
 		System.out.println(check);
 	}
 	
