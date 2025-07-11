be08782796d7b25183fa0a77ad7d4fb2b3bc058c
teacher <teacher@DESKTOP-TDDQARH>
2025-07-11 19:01:35 +0800

    web

---
+++ JavaWeb/src/main/java/filter/LoginFilter.java
@@ -0,0 +1,19 @@
+package filter;
+
+import java.io.IOException;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebFilter(urlPatterns = {"/report/*"})
+public class LoginFilter extends HttpFilter {
+	@Override
+	protected void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		response.getWriter().print("Forbidden !");
+	}
+}
