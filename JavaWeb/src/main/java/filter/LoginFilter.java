30e768310daec93bb7eef1549dc045e31a005661
teacher <teacher@DESKTOP-TDDQARH>
2025-07-11 19:13:19 +0800

    web

--- JavaWeb/src/main/java/filter/LoginFilter.java
+++ JavaWeb/src/main/java/filter/LoginFilter.java
@@ -8,15 +8,22 @@ import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 @WebFilter(urlPatterns = {"/report/*"})
 public class LoginFilter extends HttpFilter {
 	@Override
 	protected void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
-		//response.getWriter().print("Forbidden !");
 		
-		// 放行指令
-		chain.doFilter(request, response);
+		// 判斷是否有登入 ?
+		HttpSession session = request.getSession(false);
+		if(session != null && session.getAttribute("username") != null) {
+			// 放行指令
+			chain.doFilter(request, response);
+		} else {
+			response.getWriter().print("Forbidden ! Please login ~");
+		}
+		
 	}
 }
