99f5df410ebbac800fd568aac1339949ef89a1cb
teacher <teacher@DESKTOP-TDDQARH>
2025-07-14 21:03:25 +0800

    web

--- JavaWeb/src/main/java/servlet/ChatServlet.java
+++ JavaWeb/src/main/java/servlet/ChatServlet.java
@@ -17,6 +17,11 @@ import jakarta.servlet.http.HttpServletResponse;
 public class ChatServlet extends HttpServlet {
 	
 	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.getWriter().print("chat ok");
+	}
+	
+	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		String message = req.getParameter("message");
 		String ollamaResponse = "";
