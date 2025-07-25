1c9177a2c599851625fdf609c91efcd2aff88a47
teacher <teacher@DESKTOP-TDDQARH>
2025-07-25 21:22:42 +0800

    web

--- JavaWeb/src/main/java/websocket/VoteClient.java
+++ JavaWeb/src/main/java/websocket/VoteClient.java
@@ -1,12 +1,61 @@
 package websocket;
 
+import java.net.URI;
 import javax.websocket.*;
 
-
+@ClientEndpoint
 public class VoteClient {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+	private Session session;
+	
+	public VoteClient() throws Exception {
+		WebSocketContainer container = ContainerProvider.getWebSocketContainer();
+		container.connectToServer(this, URI.create("ws://localhost:8080/JavaWeb/vote/server"));
+	}
+	
+	@OnOpen
+	public void onOpen(Session session) {
+		this.session = session;
+	}
+	
+	@OnMessage
+	public void onMessage(String message) {
+		
+	}
+	
+	@OnClose
+	public void onClose(Session session) {
+		
+	}
+	
+	@OnError
+	public void onError(Session session) {
+		
+	}
+	
+	public void sendMessage(String jsonString) {
+		session.getAsyncRemote().sendText(jsonString);
+	}
+	
+	public static void main(String[] args) throws Exception  {
+		new Thread(() -> {
+			try {
+				VoteClient voteClient = new VoteClient();
+				while(true) {
+					if(Math.random() > 0.5) {
+						voteClient.sendMessage("{\"vote\":\"recall\", \"value\":1}");
+					} else {
+						voteClient.sendMessage("{\"vote\":\"nonRecall\", \"value\":1}");
+					}
+					Thread.sleep(100);
+				}
+				
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}).start();
+		
+		
 
 	}
 
