40c9a0703c02c1a5c7d912711bbfd50140d6259c
teacher <teacher@DESKTOP-TDDQARH>
2025-07-28 19:57:53 +0800

    web

--- JavaWeb/src/main/java/model/Fastfood.java
+++ JavaWeb/src/main/java/model/Fastfood.java
@@ -1,7 +1,15 @@
 package model;
 
 import lombok.Data;
-
+/*
+use web;
+create table if not exists fastfood(
+	product_id varchar(10) primary key,
+    product_name varchar(50) not null,
+    product_price int not null,
+    product_image text
+);
+ * */
 @Data
 public class Fastfood {
 	private String productId;
