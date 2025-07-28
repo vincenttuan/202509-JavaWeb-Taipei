db82e36769a5365a369bbe2de3c6cef44cbf9e84
teacher <teacher@DESKTOP-TDDQARH>
2025-07-28 20:52:39 +0800

    web

--- JavaWeb/src/main/java/servlet/UploadFastfoodServlet.java
+++ JavaWeb/src/main/java/servlet/UploadFastfoodServlet.java
@@ -1,9 +1,15 @@
 package servlet;
 
+import jakarta.servlet.annotation.MultipartConfig;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 
-@WebServlet("/")
+@WebServlet("/upload/fastfood")
+@MultipartConfig(
+		fileSizeThreshold = 1 * 1024 * 1024, // 檔案超過 1 MB 才寫入暫存檔
+		maxFileSize = 10 * 1024 * 1024 // 最大檔案不得超過 10 MB
+)
 public class UploadFastfoodServlet extends HttpServlet {
-
+	
+	
 }
