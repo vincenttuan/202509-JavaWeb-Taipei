d35295c60502b350fdf20e1e6eb75fa53fc6a3fd
teacher <teacher@DESKTOP-TDDQARH>
2025-07-28 20:34:34 +0800

    web

--- JavaWeb/src/main/java/dao/FastfoodDaoMySQL.java
+++ JavaWeb/src/main/java/dao/FastfoodDaoMySQL.java
@@ -1,5 +1,8 @@
 package dao;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 import model.Fastfood;
@@ -8,7 +11,25 @@ public class FastfoodDaoMySQL extends BaseDao implements FastfoodDao {
 
 	@Override
 	public Fastfood getFastfoodById(String productId) {
-		// TODO Auto-generated method stub
+		String sql = "select product_id, product_name, product_price, product_image from fastfood where product_id = ?";
+		try(PreparedStatement pstmt = getConnection().prepareStatement(sql)) {
+			pstmt.setString(1, productId);
+			
+			try(ResultSet rs = pstmt.executeQuery()) {
+				if(rs.next()) {
+					Fastfood fastfood = new Fastfood();
+					fastfood.setProductId(rs.getString("product_id"));
+					fastfood.setProductName(rs.getString("product_name"));
+					fastfood.setProductPrice(rs.getInt("product_price"));
+					fastfood.setProductImage(rs.getString("product_image"));
+					return fastfood;
+				}
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
 		return null;
 	}
 
