87478901f437e11c47c6dc721abdf778122d8d38
teacher <teacher@DESKTOP-TDDQARH>
2025-07-28 19:55:27 +0800

    web

---
+++ JavaWeb/src/main/java/model/Fastfood.java
@@ -0,0 +1,12 @@
+package model;
+
+import lombok.Data;
+
+@Data
+public class Fastfood {
+	private String productId;
+	private String productName;
+	private int productPrice;
+	private String productImage;
+	
+}
