14ebf2f1263cd8965f3ccedee2a40825dad1c7b4
teacher <teacher@DESKTOP-TDDQARH>
2025-07-28 20:30:35 +0800

    web

---
+++ JavaWeb/src/main/java/dao/FastfoodDaoMySQL.java
@@ -0,0 +1,33 @@
+package dao;
+
+import java.util.List;
+
+import model.Fastfood;
+
+public class FastfoodDaoMySQL extends BaseDao implements FastfoodDao {
+
+	@Override
+	public Fastfood getFastfoodById(String productId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Fastfood> findAllFastfoods() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void addFastfood(String productId, String productName, String productPrice, String productImage) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteFastfoodById(String productId) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
