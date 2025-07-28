9dc22b67e303b2ac729067e6144b114f678cdafe
teacher <teacher@DESKTOP-TDDQARH>
2025-07-28 20:49:41 +0800

    web

---
+++ JavaWeb/src/main/java/servlet/UploadFastfoodServlet.java
@@ -0,0 +1,9 @@
+package servlet;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+@WebServlet("/")
+public class UploadFastfoodServlet extends HttpServlet {
+
+}
